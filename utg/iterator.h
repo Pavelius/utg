bcd7ee22bd18b4a8ca980a047269eccab5f9fef4
Pavelius <p000000000001@gmail.com>
2022-02-03 09:15:04 +0200

    Исправил интерфейс итератора

--- utg/iterator.h
+++ utg/iterator.h
@@ -5,19 +5,22 @@
 typedef unsigned short indext;
 
 template<typename T, typename V = indext>
+class iterable {
+	const V* data;
+public:
+	constexpr iterable(const V* data) : data(data) {}
+	constexpr bool operator!=(const iterable& v) const { return data != v.data; }
+	constexpr T* operator*() const { return bsdata<T>::elements + *data; }
+	constexpr void operator++() { data++; }
+};
+template<typename T, typename V = indext>
 class iterator {
-	struct element {
-		const V* data;
-		constexpr bool operator!=(const element& v) const { return data != v.data; }
-		constexpr T* operator*() const { return bsdata<T>::elements + *data; }
-		constexpr void operator++() { data++; }
-	};
 	const V* data;
 	size_t count;
 public:
 	constexpr iterator(const V* v, size_t count) : data(v), count(count) {}
 	template<size_t N> constexpr iterator(const adat<V, N>& v) : data(v.data), count(v.count) {}
 	template<size_t N> constexpr iterator(char(&v)[N]) : data(v), count(N) {}
-	element	begin() const { return {data}; }
-	element	end() const { return {data + count}; }
+	iterable<T, V> begin() const { return data; }
+	iterable<T, V> end() const { return data + count; }
 };
